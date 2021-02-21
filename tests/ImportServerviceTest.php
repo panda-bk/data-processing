c479d3424253ee8f4d1a181da9076afcb6d8d6ee
David Richard <davidrichard.ms@gmail.com>
2021-02-21 12:58:48 -0300

    crete new test

--- tests/ImportServerviceTest.php
+++ tests/ImportServerviceTest.php
@@ -7,22 +7,19 @@ test('validade type import', function () {
     $importService = new \App\Services\ImportService();
     $salesmanData = ['001', '08625455956', 'David Richard', '5800'];
     $customerData = ['002', '70241790000182', 'Check List Facil', 'develop'];
-    $salesData = ['003','08','[1­34­10, 2­33­1. 50, 3­40­0. 10]' ,'Elias'];
+    $salesData = "003,10,[1­10­100, 2­30­2. 50, 3­40­3. 10] ,Steve";
 
     expect($importService->getTypeId($salesmanData[0]))->toEqual(TypeEnumAlias::SALESMAN);
     expect($importService->getTypeId($customerData[0]))->toEqual(TypeEnumAlias::CUSTOMER);
-    expect($importService->getTypeId($salesData[0]))->toEqual(TypeEnumAlias::SALES);
+    expect($importService->getTypeId('003'))->toEqual(TypeEnumAlias::SALES);
 
     expect($importService->importSalesman($salesmanData))->toBeInstanceOf(\App\Domain\Salesman\Salesman::class);
     expect($importService->importCustomer($customerData))->toBeInstanceOf(\App\Domain\Customer\Customer::class);
-    //expect($importService->importSales($salesmanData))->toBeInstanceOf(\App\Domain\Sales\Sales::class);
+    expect($importService->extractSales($salesData))->toBeInstanceOf(\App\Domain\Sales\Sales::class);
 
+    $items = '1­10­100, 2­30­2. 50, 3­40­3. 10';
+    $item = ['1', '10', '100'];
 
-
-    //expect($importService->importSalesItems($salesData[2]))->toBeInstanceOf(SaleItemsCollecion::class);
-
-
-
-
-
+    expect($importService->importItems($item))->toBeInstanceOf(\App\Domain\Sales\SaleItem::class);
+    expect($importService->extractSalesItem($items))->toBeInstanceOf(\Doctrine\Common\Collections\ArrayCollection::class);
 });
